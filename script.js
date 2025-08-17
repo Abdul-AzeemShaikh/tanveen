32641ca1a3ea8f229f09b3617427a89dabff86a5
A-AzeemS. <aamas1577@gmail.com>
2025-08-17 08:47:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,448 @@
+// Fungsi untuk memulai musik
+function playMusic() {
+  const music = document.getElementById('background-music');
+  music.play();
+}
+window.addEventListener('DOMContentLoaded', function() {
+  playMusic();
+});
+document.body.addEventListener('click', playMusic, { once: true });
+const content = document.getElementById('content');
+const footer = document.getElementsByTagName('footer')[0];
+const timer = document.getElementById('timer');
+
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+let countDown = new Date('Jan 09, 2026 00:00:00').getTime(),
+  x = setInterval(function () {
+    let now = new Date().getTime(),
+      distance = countDown - now;
+    document.getElementById('days').innerText = Math.floor(distance / (day)),
+    document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+    if (distance < 0) {
+
+      timer.classList.add('d-none');
+      confetti();
+      clearInterval(x);
+      _slideSatu();
+    }
+
+  }, second)
+
+const _slideSatu = function () {
+  const tap = document.getElementById('tap');
+  const slideSatu = document.getElementById('slideSatu');
+  slideSatu.classList.remove('d-none');
+  setTimeout(function () {
+    tap.classList.remove('d-none');
+    document.body.addEventListener('click', function () {
+      _slideDua();
+    })
+  }, 7000);
+};
+
+const _slideDua = function () {
+  const slideSatu = document.getElementById('slideSatu');
+  const tap = document.getElementById('tap');
+  const slideDua = document.getElementById('slideDua');
+
+  setTimeout(function () {
+    slideSatu.classList.replace('animate__slideInDown', 'animate__backOutDown');
+    tap.classList.add('d-none');
+    setTimeout(function () {
+      slideSatu.classList.add('d-none');
+    }, 1000);
+  }, 1000);
+
+  slideDua.classList.remove('d-none');
+  setTimeout(function () {
+    tap.classList.remove('d-none');
+    document.body.addEventListener('click', function () {
+      slideDua.classList.replace('animate__zoomInDown', 'animate__fadeOutLeft');
+      slideDua.classList.remove('animate__delay-2s', 'animate__slow');
+      tap.classList.add('d-none');
+      setTimeout(function () {
+        slideDua.remove();
+        _slideTiga();
+      }, 1000);
+    })
+  }, 40000);
+};
+
+const _slideTiga = function () {
+  const tap = document.getElementById('tap');
+  const slideTiga = document.getElementById('slideTiga');
+
+  slideTiga.classList.remove('d-none');
+  setTimeout(function () {
+    tap.classList.remove('d-none');
+    document.body.addEventListener('click', function () {
+      slideTiga.classList.remove('animate__delay-2s', 'animate__slow');
+      slideTiga.classList.replace('animate__fadeInRight', 'animate__fadeOut');
+      tap.remove();
+      setTimeout(function () {
+        slideTiga.remove();
+        _slideEmpat();
+      }, 1000);
+    })
+  }, 43000);
+}
+
+function getRandomPosition(element) {
+  var x = document.body.offsetHeight - element.clientHeight;
+  var y = document.body.offsetWidth - element.clientWidth;
+  var randomX = Math.floor(Math.random() * 500);
+  var randomY = Math.floor(Math.random() * y);
+  return [randomX, randomY];
+};
+
+const _slideEmpat = function () {
+  const slideEmpat = document.getElementById('slideEmpat');
+  const btn = document.getElementsByTagName('button');
+  slideEmpat.classList.remove('d-none');
+
+  btn[0].addEventListener('click', function () {
+    var xy = getRandomPosition(slideEmpat);
+    slideEmpat.style.top = xy[0] + 'px';
+    // slideEmpat.style.left = xy[1] + 'px';
+  });
+
+  btn[1].addEventListener('click', function () {
+    slideEmpat.classList.replace('animate__fadeInDown', 'animate__bounceOut');
+    slideEmpat.classList.remove('animate__delay-2s');
+    setTimeout(function () {
+      slideEmpat.remove()
+      setTimeout(() => {
+        _slideLima();
+      }, 500);
+    }, 1000);
+  })
+};
+
+const _slideLima = function () {
+  const slideLima = document.getElementById('slideLima');
+  slideLima.classList.remove('d-none');
+  const trims = document.getElementById('trims');
+
+  setTimeout(() => {
+    trims.classList.remove('d-none');
+  }, 1000);
+
+  slideLima.addEventListener('animationend', () => {
+    slideLima.classList.add('animate__delay-3s')
+    slideLima.classList.replace('animate__bounceIn', 'animate__fadeOut');
+    trims.classList.add('animate__animated', 'animate__fadeOut', 'animate__delay-3s');
+    setTimeout(() => {
+      trims.remove();
+      setTimeout(() => {
+        slideLima.remove();
+        _slideEnam();
+      }, 1000);
+    }, 6000);
+  });
+};
+
+const _slideEnam = function () {
+  const slideEnam = document.getElementById('slideEnam');
+  slideEnam.classList.remove('d-none');
+};
+
+
+new TypeIt("#teks1", {
+  strings: ["Today, I send all my best prayers for you. May the things that make you fall also become your reasons to keep growing. May your days always be accompanied by love that never has limits. May every step you take be easy until you achieve your desired goals. With or without me, may the universe always make you happy, no matter what. Since the 24th of April, you have been a very amusing person to talk to. I was always so open to you; you just had something in you that made me trust you. On 24th April, Thursday 2025, we sat down together, idk why 🤣 talked for 1-2 hours, idk why, but yes, we got to know each other very well. From that day to the last day of school, we were always together, or maybe I was always with you. We studied together for all the subjects got okaish marks but I believe that if i hadn’t studied with you i would not ever get the grade I got the grade i got is because of you you are such a sweet little girl❤️ one thing i didnt tell you was that for one of the exams i wrote something about you using your nickname tanna🤣❤️. Sorry for not coming and meeting you after exams, I wanted to, btw, you wanna hear something? After exams, I went to Mumbai right for my passport. Till now I haven’t got it🤣. After that, I went to Mumbai, so for like 1 week we stopped talking to each other, but once you got WhatsApp, we again started to talk like day-and-night🤣. I mean, idk I had some sort of connection with you. We told each other our secrets, and most importantly, you wrote that cute little letter for me, which I won’t ever forget about❤️ I still read it sometimes🤣."],
+  startDelay: 4000,
+  speed: 40,
+  waitUntilVisible: true
+}).go();
+
+new TypeIt("#teks2", {
+  strings: ["You were someone different from everyone else…Then we used to talk to each other, share every single thing in our lives when you were in Jharkhand & I was in Mumbai. We gave each other soooo many flowers, idk why🤣, and I had a very strong bond with you. Remember those days where we woke up at 4 am to study together, I won’t forget that, and remember those days where we would just care for each other so much, like idk it was like we were there for each other every single time. The messages I used to send you at 3 a.m. 🤣 and you still responded to them ❤️. And honestly, there is nothing for me to hate about you. Yes, we do sometimes fight, but like that’s just coz we’re bored😜 don’t worry. And one more thing before I end this, I am sorry for ALL the bad things I might have done to you…😔😔😔  I am always willing to call you, but sadly, I can’t😔 if I get time to I will call you one day and we will talk. And I’m so bad, just want to hug you🤣. And at the point in the USA, you seem very busy. I think about you a lot, and I don’t know what to do about that. But it’s okay.  Last thing, could you please tell me how comfortable you are telling me your stuff? I always tell you what is going on in my head and heart. Remember, I wrote a long message to you some day. In that message, I was trying to tell you to be free with me and tell me everything. And remember, you also told me to tell you everything, + i also told you that mostly I journal my thoughts. You told me to tell you my thoughts rather than journal it, but now you don’t feel really good telling me your stuff, and it’s fine."],
+  startDelay: 4000,
+  speed: 40,
+  waitUntilVisible: true
+}).go();
+
+
+new TypeIt("#trims", {
+  strings: ["Thank you for every single thing you, as a cute lil sweetie lil lovely lil girl you did for me. Is there some word that is maybe better than thank you, you deserve it.🌸"],
+  startDelay: 1000,
+  speed: 40,
+  loop: false,
+  waitUntilVisible: true,
+}).go();
+
+
+
+'use strict';
+
+var onlyOnKonami = false;
+
+function confetti() {
+  // Globals
+  var $window = $(window),
+    random = Math.random,
+    cos = Math.cos,
+    sin = Math.sin,
+    PI = Math.PI,
+    PI2 = PI * 2,
+    timer = undefined,
+    frame = undefined,
+    confetti = [];
+
+  var runFor = 2000
+  var isRunning = true
+
+  setTimeout(() => {
+    isRunning = false
+  }, runFor);
+
+  // Settings
+  var konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+    pointer = 0;
+
+  var particles = 150,
+    spread = 20,
+    sizeMin = 5,
+    sizeMax = 12 - sizeMin,
+    eccentricity = 10,
+    deviation = 100,
+    dxThetaMin = -.1,
+    dxThetaMax = -dxThetaMin - dxThetaMin,
+    dyMin = .13,
+    dyMax = .18,
+    dThetaMin = .4,
+    dThetaMax = .7 - dThetaMin;
+
+  var colorThemes = [
+    function () {
+      return color(200 * random() | 0, 200 * random() | 0, 200 * random() | 0);
+    },
+    function () {
+      var black = 200 * random() | 0;
+      return color(200, black, black);
+    },
+    function () {
+      var black = 200 * random() | 0;
+      return color(black, 200, black);
+    },
+    function () {
+      var black = 200 * random() | 0;
+      return color(black, black, 200);
+    },
+    function () {
+      return color(200, 100, 200 * random() | 0);
+    },
+    function () {
+      return color(200 * random() | 0, 200, 200);
+    },
+    function () {
+      var black = 256 * random() | 0;
+      return color(black, black, black);
+    },
+    function () {
+      return colorThemes[random() < .5 ? 1 : 2]();
+    },
+    function () {
+      return colorThemes[random() < .5 ? 3 : 5]();
+    },
+    function () {
+      return colorThemes[random() < .5 ? 2 : 4]();
+    }
+  ];
+
+  function color(r, g, b) {
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
+
+  // Cosine interpolation
+  function interpolation(a, b, t) {
+    return (1 - cos(PI * t)) / 2 * (b - a) + a;
+  }
+
+  // Create a 1D Maximal Poisson Disc over [0, 1]
+  var radius = 1 / eccentricity,
+    radius2 = radius + radius;
+
+  function createPoisson() {
+    // domain is the set of points which are still available to pick from
+    // D = union{ [d_i, d_i+1] | i is even }
+    var domain = [radius, 1 - radius],
+      measure = 1 - radius2,
+      spline = [0, 1];
+    while (measure) {
+      var dart = measure * random(),
+        i, l, interval, a, b, c, d;
+
+      // Find where dart lies
+      for (i = 0, l = domain.length, measure = 0; i < l; i += 2) {
+        a = domain[i], b = domain[i + 1], interval = b - a;
+        if (dart < measure + interval) {
+          spline.push(dart += a - measure);
+          break;
+        }
+        measure += interval;
+      }
+      c = dart - radius, d = dart + radius;
+
+      for (i = domain.length - 1; i > 0; i -= 2) {
+        l = i - 1, a = domain[l], b = domain[i];
+        // c---d          c---d  Do nothing
+        //   c-----d  c-----d    Move interior
+        //   c--------------d    Delete interval
+        //         c--d          Split interval
+        //       a------b
+        if (a >= c && a < d)
+          if (b > d) domain[l] = d; // Move interior (Left case)
+          else domain.splice(l, 2); // Delete interval
+        else if (a < c && b > c)
+          if (b <= d) domain[i] = c; // Move interior (Right case)
+          else domain.splice(i, 0, c, d); // Split interval
+      }
+
+      for (i = 0, l = domain.length, measure = 0; i < l; i += 2)
+        measure += domain[i + 1] - domain[i];
+    }
+
+    return spline.sort();
+  }
+
+  var container = document.createElement('div');
+  container.style.position = 'fixed';
+  container.style.top = '0';
+  container.style.left = '0';
+  container.style.width = '100%';
+  container.style.height = '0';
+  container.style.overflow = 'visible';
+  container.style.zIndex = '9999';
+
+  // Confetto constructor
+  function Confetto(theme) {
+    this.frame = 0;
+    this.outer = document.createElement('div');
+    this.inner = document.createElement('div');
+    this.outer.appendChild(this.inner);
+
+    var outerStyle = this.outer.style,
+      innerStyle = this.inner.style;
+    outerStyle.position = 'absolute';
+    outerStyle.width = (sizeMin + sizeMax * random()) + 'px';
+    outerStyle.height = (sizeMin + sizeMax * random()) + 'px';
+    innerStyle.width = '100%';
+    innerStyle.height = '100%';
+    innerStyle.backgroundColor = theme();
+
+    outerStyle.perspective = '50px';
+    outerStyle.transform = 'rotate(' + (360 * random()) + 'deg)';
+    this.axis = 'rotate3D(' +
+      cos(360 * random()) + ',' +
+      cos(360 * random()) + ',0,';
+    this.theta = 360 * random();
+    this.dTheta = dThetaMin + dThetaMax * random();
+    innerStyle.transform = this.axis + this.theta + 'deg)';
+
+    this.x = $window.width() * random();
+    this.y = -deviation;
+    this.dx = sin(dxThetaMin + dxThetaMax * random());
+    this.dy = dyMin + dyMax * random();
+    outerStyle.left = this.x + 'px';
+    outerStyle.top = this.y + 'px';
+
+    // Create the periodic spline
+    this.splineX = createPoisson();
+    this.splineY = [];
+    for (var i = 1, l = this.splineX.length - 1; i < l; ++i)
+      this.splineY[i] = deviation * random();
+    this.splineY[0] = this.splineY[l] = deviation * random();
+
+    this.update = function (height, delta) {
+      this.frame += delta;
+      this.x += this.dx * delta;
+      this.y += this.dy * delta;
+      this.theta += this.dTheta * delta;
+
+      // Compute spline and convert to polar
+      var phi = this.frame % 7777 / 7777,
+        i = 0,
+        j = 1;
+      while (phi >= this.splineX[j]) i = j++;
+      var rho = interpolation(
+        this.splineY[i],
+        this.splineY[j],
+        (phi - this.splineX[i]) / (this.splineX[j] - this.splineX[i])
+      );
+      phi *= PI2;
+
+      outerStyle.left = this.x + rho * cos(phi) + 'px';
+      outerStyle.top = this.y + rho * sin(phi) + 'px';
+      innerStyle.transform = this.axis + this.theta + 'deg)';
+      return this.y > height + deviation;
+    };
+  }
+
+
+  function poof() {
+    if (!frame) {
+      // Append the container
+      document.body.appendChild(container);
+
+      // Add confetti
+
+      var theme = colorThemes[onlyOnKonami ? colorThemes.length * random() | 0 : 0],
+        count = 0;
+
+      (function addConfetto() {
+
+        if (onlyOnKonami && ++count > particles)
+          return timer = undefined;
+
+        if (isRunning) {
+          var confetto = new Confetto(theme);
+          confetti.push(confetto);
+
+          container.appendChild(confetto.outer);
+          timer = setTimeout(addConfetto, spread * random());
+        }
+      })(0);
+
+
+      // Start the loop
+      var prev = undefined;
+      requestAnimationFrame(function loop(timestamp) {
+        var delta = prev ? timestamp - prev : 0;
+        prev = timestamp;
+        var height = $window.height();
+
+        for (var i = confetti.length - 1; i >= 0; --i) {
+          if (confetti[i].update(height, delta)) {
+            container.removeChild(confetti[i].outer);
+            confetti.splice(i, 1);
+          }
+        }
+
+        if (timer || confetti.length)
+          return frame = requestAnimationFrame(loop);
+
+        // Cleanup
+        document.body.removeChild(container);
+        frame = undefined;
+      });
+    }
+  }
+
+  $window.keydown(function (event) {
+    pointer = konami[pointer] === event.which ?
+      pointer + 1 :
+      +(event.which === konami[0]);
+    if (pointer === konami.length) {
+      pointer = 0;
+      poof();
+    }
+  });
+
+  if (!onlyOnKonami) poof();
+};
